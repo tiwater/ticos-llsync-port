36f76e04d0e81e0b5c3a86dbb8dd5bea677b767b
whstart <1065510910@qq.com>
2022-11-18 14:15:05 +0800

    fix: 修复ll_task线程过小导致的crash问题

--- data_template/ble_qiot_template.h
+++ data_template/ble_qiot_template.h
@@ -263,7 +263,7 @@ typedef int (*property_array_set_cb)(const char *data, uint16_t len, uint16_t in
 typedef int (*property_array_get_cb)(char *buf, uint16_t buf_len, uint16_t index);
 
 
-//#define	BLE_QIOT_INCLUDE_EVENT
+#define	BLE_QIOT_INCLUDE_EVENT
 
 // define event id
 enum {
@@ -318,7 +318,7 @@ typedef struct{
 }ble_event_t;
 
 
-//#define	BLE_QIOT_INCLUDE_ACTION
+#define	BLE_QIOT_INCLUDE_ACTION
 
 // define action id
 enum {
